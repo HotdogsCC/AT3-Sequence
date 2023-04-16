32485d04db9745e3a74fd9eb388e0efe58048dad
HotdogsCC <chacha9669@hotmail.com>
2023-04-16 23:30:10 +1000

    Board Highlights
    temp start scene and a game scene. game scene has a game board where the tiles highlight. it's pretty cool

---
+++ Assets/Scripts/TileHighlight.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TileHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image image;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    private void OnMouseOver()
+    {
+        Debug.Log("Success!");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.color = new Color(0f, 0.3402588f, 1f, 0.2392157f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.color = Color.clear;
+    }
+}
